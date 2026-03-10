acc027580c53eddc7e736b39076007f135bd0483
amitpaswan202 <paswanamit512@gmail.com>
2026-03-10 19:52:45 +0530

    few changes in js

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ let boxSize = 30;
 let rows = Math.floor(boardHeight / boxSize);
 let cols = Math.floor(boardWidth / boxSize);
 
-for(let i = 0; i < rows * cols; i++){
+// for(let i = 0; i < rows * cols; i++){
+//     let box = document.createElement("div");
+//     box.classList.add("box");
+//     board.appendChild(box);
+// }
+
+for(let row = 0 ;row < rows ;row++){
+    for(let col = 0;col < cols ;col++){
     let box = document.createElement("div");
     box.classList.add("box");
     board.appendChild(box);
+    }
+
 }
