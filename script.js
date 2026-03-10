41eeef0b60d3943588aef38ddedd892a783c325b
amitpaswan202 <paswanamit512@gmail.com>
2026-03-10 19:35:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let board = document.querySelector(".board");
+
+let boardHeight = board.clientHeight;
+let boardWidth = board.clientWidth;
+
+let boxSize = 30;
+
+let rows = Math.floor(boardHeight / boxSize);
+let cols = Math.floor(boardWidth / boxSize);
+
+for(let i = 0; i < rows * cols; i++){
+    let box = document.createElement("div");
+    box.classList.add("box");
+    board.appendChild(box);
+}
